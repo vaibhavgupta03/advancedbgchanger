8c1bce5441a06f21cafc21906c5ad45e1d790d74
Vaibhav Gupta <vaibhavgupta6393@gmail.com>
2023-10-19 14:34:49 +0530

    css updated

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -6,10 +6,12 @@ function App() {
   return (
       <div className="w-full h-screen duration-100"
       style={{ backgroundColor: color }}>
-      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 duration-1'>
-        <div className='w-100 h-screen flex justify-center items-center gap-5'>
-          <input type='color' id='colorPicker' onMouseOver={() => setColor(document.getElementById("colorPicker").value)}></input>
-          {/* <button onClick={() => setColor(document.getElementById("colorPicker").value)} className='outline-none px-5 py-3 rounded-full text-white shadow-lg bg-fuchsia-700 hover:bg-fuchsia-900'>Change</button> */}
+      <div className='flex flex-wrap justify-center py-40 px-2 duration-100'>
+        <div className='w-100 h-60 flex flex-col justify-center items-center gap-5 bg-white rounded-2xl p-4'>
+          <h1 className='text-5xl font-bold'>Background Changer</h1>
+          <div className='flex gap-3'>
+          <label className='text-lg font-semibold text-yellow-950'>Choose your Background Colour</label>
+          <input type='color' id='colorPicker' className='rounded-lg bg-white px-1 border-2 border-black' onChange={(e) => setColor(e.target.value)}></input></div>
           </div>
       </div>
       </div>
